9cc5124dbd461973d3cb92082a57eb3b39613513
Tatiane Medeiros <tatianeportelamedeiros@gmail.com>
2024-03-19 18:36:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.png'],
+  build: {
+    assetsDir: 'assets'
+  }
 })
