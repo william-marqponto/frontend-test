933678cb526a0c9b4abc1be771037a8c6f1a9bff
Tatiane Medeiros <tatianeportelamedeiros@gmail.com>
2024-03-19 11:24:56 -0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Container } from './Header.ts';
 export function Header() {
   return (
     <Container>
-      <img src='src/assets/pokemon.png' alt='Logo Pokémon' />
+      <img src='../assets/pokemon.png' alt='Logo Pokémon' />
     </Container>
   );
 }
